1524bed66dd658ed642c7f47b63394fcb2b13856
dupeiyi-40308 <dupyi0912@gmail.com>
2022-08-09 16:07:48 +0800

    ✨ feat(添加头部用户的一些提示组件): 头部右侧组件的编写
    加入头部右侧组件

--- src/layouts/LayoutHeader/index.jsx
+++ src/layouts/LayoutHeader/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from 'choerodon-ui';
 import HeaderLogo from './components/HeaderLogo';
 import HeaderMenu from './components/HeaderMenu';
+import HeaderUser from './components/HeaderUser';
 import CSearch from '../../components/CSearch';
 import styles from './index.module.less';
 
@@ -10,15 +11,18 @@ const { Header } = Layout;
 // 头部布局组件
 const LayoutHeader = () => {
     return (
-        <Header className={styles['layout-header-container']} style={{ backgroundColor: '#fff' }}>
+        <Header className={styles['layout-header-container']}>
             <div className={styles['layout-header-logo']}>
                 <HeaderLogo />
             </div>
             <div className={styles['layout-header-search']}>
                 <CSearch />
             </div>
-            <div>
+            <div className='flex-1'>
                 <HeaderMenu />
+            </div>
+            <div>
+                <HeaderUser />
             </div>
         </Header>
     );
